960132d6a2025572b4a83d753157733335509a4a
Codepocketdev <artsmartin44@gmail.com>
2025-12-26 09:19:17 +0300

    Initial SatsPOS Lightning POS app

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'satspos-v1';
+const urlsToCache = [
+  '/pos_bluetooth.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
